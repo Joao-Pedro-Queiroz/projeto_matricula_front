cb826f65a179670235e11dcd7355e201502a879f
Joao-Pedro-Queiroz <jpqv0105@gmail.com>
2025-04-29 08:55:05 -0300

    Configurações Authh0 pronta

--- projeto_matricula/src/main.jsx
+++ projeto_matricula/src/main.jsx
@@ -4,7 +4,14 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Auth0Provider
+      domain="dev-vlbhbshl7b1pxe8e.us.auth0.com"
+      clientId="eo8WNkaTwrcGujuCMSkRZG3yMYuQvXTy"
+      authorizationParams={{
+        audience: "https://dev-vlbhbshl7b1pxe8e.us.auth0.com/api/v2/",
+        redirect_uri: window.location.origin
+      }}
+    >
     <App />
-  </StrictMode>,
+  </Auth0Provider>,
 )
