55551eb9821bb0aba91c5dae5b5f195cd5d236ca
luisebastos <luise@gustavobastos.com.br>
2025-04-30 10:53:38 -0300

    init

---
+++ projeto_matricula/src/matricula.jsx
@@ -0,0 +1,10 @@
+function Matricula() {
+    return (
+      <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <h1>Gerenciamento de Matrículas</h1>
+      </div>
+    )
+  }
+  
+  export default Matricula
+  
